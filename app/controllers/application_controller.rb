804711efb59fbac8c2ccf58c541f9f73d0b675dd
Shaun <ycorredius@gmail.com>
2019-11-01 11:19:44 -0400

    Welcome page rough draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "my_application_secret"
   end
 
   get "/" do
     erb :welcome
   end
-
-  post "/" do
-    binding.pry 
-    first = params[:name].split(" ")[0]
-    last = params[:name].split(" ")[1]
-    @user = User.create(first: first,last: last, email: params[:email],phone_number:params[:phone_number] )
-  end
+  
 end
